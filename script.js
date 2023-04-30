7c4b84be5d700c572c85b5cabf62938214041bd4
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-04-30 18:55:18 -0400

    Replaced to do comments with comments that explain the functionality of the code in the JavaScript file

--- script.js
+++ script.js
@@ -1,18 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// Runs all of the following code after the webpage finishes loading
 $(function () {
+  //Stores divs, classes and the current time to be referenced later
   const timeBlock = $('.time-block');
   const currentHour = dayjs().format('H');
   const timeTags = ['#hour-9', '#hour-10', '#hour-11', '#hour-12', '#hour-13', '#hour-14', '#hour-15', '#hour-16', '#hour-17']
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
+  // Listens for when one of the planner divs is clicked and saves the text in the text area to local storage if the save button was clicked
   timeBlock.on('click', function(e) {
     const event = e.target;
     if (event.matches('button')) {
@@ -22,12 +15,7 @@ $(function () {
     }
   })
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  
+  // Iterates through the timeTags array and compares the number hour to the current time to determine if the past. present or future class should be added to the corresponding div  
   for (x=0; x<timeTags.length; x++) {
     const tag = timeTags[x];
     const match = tag.match(/\d+/);
@@ -42,16 +30,13 @@ $(function () {
     }
   }
 
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  
+  // Retrieves data from local storage and puts it into the planner
   for (x=0; x<timeTags.length; x++) {
     const local = localStorage.key(x);
     const plannerText = localStorage.getItem(local);
     $(timeTags[x]).children('.description').val(plannerText);
   }
 
-  // TODO: Add code to display the current date in the header of the page.
+  // Displays the current date and day of the week in the header
   $('#currentDay').text(dayjs().format('dddd, MMM D'));
 });
